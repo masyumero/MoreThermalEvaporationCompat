b14ecd18d8527cd629edb0a00c1247122188460e
masyumero <167562220+masyumero@users.noreply.github.com>
2025-12-29 08:03:21 +0900

    Add JEI integration for More Thermal Evaporation Compat

---
+++ src/main/java/io/github/masyumero/morethermalevaporationcompat/client/jei/MTECompatJEI.java
@@ -0,0 +1,35 @@
+package io.github.masyumero.morethermalevaporationcompat.client.jei;
+
+import io.github.masyumero.morethermalevaporationcompat.MoreThermalEvaporationCompat;
+import io.github.masyumero.morethermalevaporationcompat.common.content.blocktype.MTECompatMultiPartType;
+import io.github.masyumero.morethermalevaporationcompat.common.registries.MoreThermalEvaporationCompatBlocks;
+import io.github.masyumero.morethermalevaporationcompat.common.tier.TETier;
+import mekanism.client.jei.CatalystRegistryHelper;
+import mekanism.client.jei.MekanismJEIRecipeType;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.resources.ResourceLocation;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@JeiPlugin
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class MTECompatJEI implements IModPlugin {
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return MoreThermalEvaporationCompat.rl("jei_plugin");
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registry) {
+        for (TETier tier : TETier.values()) {
+            for (MTECompatMultiPartType type : MTECompatMultiPartType.values()) {
+                if (tier.isModLoaded()) CatalystRegistryHelper.register(registry, MekanismJEIRecipeType.EVAPORATING, MoreThermalEvaporationCompatBlocks.getBlock(tier, type));
+            }
+        }
+    }
+}
